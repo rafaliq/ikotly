1e7157f34ea3101c66e7f805baaf4e74642bc897
Rafał <rafal@MacBook-Pro-mac.local>
2020-03-27 14:34:55 +0100

    offer

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -21,7 +21,7 @@
 </section>
 
 <section class="section">
-  <div class="container offer">
+  <div class="container">
     <div class="section-info">
       <h2>
         <span class="pretitle">
@@ -34,6 +34,47 @@
       <p class="text section-info__desc">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dolor felis, elementum at cursus nec, convallis id ex.
       </p>
+    </div>
+    <div class="offer">
+      <div class="offer__column">
+        <div class="offer__box">
+          <h3 class="title title--small offer__title">
+            Kotły na pelet
+          </h3>
+          <p class="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dolor felis, elementum at cursus nec, convallis id ex. Etiam vel tempus ex.
+          </p>
+        </div>
+        <div class="offer__box">
+          <h3 class="title title--small offer__title">
+            Fotowoltaika
+          </h3>
+          <p class="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dolor felis, elementum at cursus nec, convallis id ex. Etiam vel tempus ex.
+          </p>
+        </div>
+      </div>
+      <div class="offer__image">
+        <img src="@asset('images/boiler.png')" alt="kocioł">
+      </div>
+      <div class="offer__column">
+        <div class="offer__box">
+          <h3 class="title title--small offer__title">
+            Pompy ciepła
+          </h3>
+          <p class="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dolor felis, elementum at cursus nec, convallis id ex. Etiam vel tempus ex.
+          </p>
+        </div>
+        <div class="offer__box">
+          <h3 class="title title--small offer__title">
+            Rekuperacja
+          </h3>
+          <p class="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dolor felis, elementum at cursus nec, convallis id ex. Etiam vel tempus ex.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </section>
