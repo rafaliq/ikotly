b65f7ddcba25e0df7927e727b88ba5c71648d16f
Rafał <rafal@MacBook-Pro-mac.local>
2020-04-07 15:49:21 +0200

    boxes rwd

--- resources/views/components/boxes.blade.php
+++ resources/views/components/boxes.blade.php
@@ -1,15 +1,15 @@
 <section class="container boxes">
   @foreach($data['boxes'] as $item)
-  <div class="boxes__item">
-    <h3 class="title">
-      {{ $item['title'] }}
-    </h3>
-    <div class="text boxes__text">
-      {!! $item['desc'] !!}
+    <div class="boxes__item">
+      <h3 class="title">
+        {{ $item['title'] }}
+      </h3>
+      <div class="text boxes__text">
+        {!! $item['desc'] !!}
+      </div>
+      <a href="{{ $item['link']['url'] }}" class="button">
+        Więcej
+      </a>
     </div>
-    <a href="{{ $item['link']['url'] }}" class="button">
-      Więcej
-    </a>
-  </div>
   @endforeach
 </section>
